e154269ac66e903e3b7fd1f90b709084df5b8e03
akshay183 <sharmahoney398@gmail.com>
2023-07-21 00:55:26 +0530

    template added

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from first_app import views
 
 urlpatterns = [
-    path("/hello", views.index),
+    path("", views.index),
 ]
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, world')
+    render_dict = {
+        'insert': "see Django is easy!",
+    }
+    return render(request, 'first_app/index.html', context=render_dict)
